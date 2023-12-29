fef59fb3dadefc0f70c0381ca2d3762312dab32d
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-29 16:53:26 -0500

    Arquitectura cliente servidor Se usa la arquitectura cliente servidor Funciona correctamente

--- activation.php
+++ activation.php
@@ -1,5 +1,6 @@
 <?php
-include 'db.php';
+define('AUTHORIZED_SCRIPT', true);
+include_once 'db.php';
 $msg = '';
 
 if (!empty($_GET['code']) && isset($_GET['code'])) {
@@ -7,16 +8,36 @@ if (!empty($_GET['code']) && isset($_GET['code'])) {
     $code = mysqli_real_escape_string($connection, $_GET['code']);
 
     // Verifica si hay un usuario con ese código en la base de datos
-    $c = mysqli_query($connection, "SELECT id_usuario, status FROM usuarios WHERE activacion='$code'");
+    $urlComprobar = 'http://192.168.1.4/BackEnd/comprobarAct/' . $code;
+    $jsonResponse = file_get_contents($urlComprobar);
+    $data = json_decode($jsonResponse, true);
+    //$c = mysqli_query($connection, "SELECT id_usuario, status FROM usuarios WHERE activacion='$code'");
 
-    if (mysqli_num_rows($c) > 0) {
-        $userData = mysqli_fetch_assoc($c);
-        $userId = $userData['id_usuario'];
-        $userStatus = $userData['status'];
+    if (count($data) > 0) {
+        foreach ($data as $user) {
+            $userId = $user['id_usuario'];
+            $userStatus = $user['status'];
+        }
+        //$userData = mysqli_fetch_assoc($c);
+        //$userId = $userData['id_usuario'];
+        //$userStatus = $userData['status'];
 
         if ($userStatus == '0') {
             // Si el usuario está inactivo, actualiza el estado a activo
-            mysqli_query($connection, "UPDATE usuarios SET status='1' WHERE id_usuario='$userId'");
+            $urlActivar = 'http://192.168.1.4/BackEnd/activar/';
+            $datos = array(
+                'id' => $userId
+            );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $urlActivar);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+            $respuesta = curl_exec($ch);
+            curl_close($ch);
+            //mysqli_query($connection, "UPDATE usuarios SET status='1' WHERE id_usuario='$userId'");
             $msg = "Tu cuenta ha sido activada";
         } else {
             $msg = "Tu cuenta ya está activa, no es necesario activarla nuevamente";

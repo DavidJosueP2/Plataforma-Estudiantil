2ad2bfaecfb0570189cd8a31778383e6f32de98e
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 16:46:01 -0500

    Añadida la ip del back como variable

---
+++ configs.php
@@ -0,0 +1,6 @@
+<?php
+    // Definir una variable global
+    $_GLOBALS['sectionHeader'] = "Sección";
+    $base_url = 'http://localhost/Proyecto-Fundamentos/';
+    $ip = 'http://192.168.1.4/BackEnd/';
+?>

e04eb0bc4591b351bf49d791527367c790343a3f
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 17:03:59 -0500

    Añadida funcion activar para usar Se usa activar en la activacion.php, usando configs.php

---
+++ funcBack/activar.php
@@ -0,0 +1,15 @@
+<?php
+function activar($datos, $ip){
+    $urlActivar = $ip.'activar';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $urlActivar);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($datos));
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    return $respuesta;
+}
+?>

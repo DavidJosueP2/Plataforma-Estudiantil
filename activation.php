baac35beb46d8d7b880c95857976203a144f248c
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 17:05:07 -0500

    Conexion con el Back mediante funciones

--- activation.php
+++ activation.php
@@ -1,6 +1,7 @@
 <?php
 define('AUTHORIZED_SCRIPT', true);
-include_once 'db.php';
+include_once 'funcBack/activar.php';
+include_once 'configs.php';
 $msg = '';
 
 if (!empty($_GET['code']) && isset($_GET['code'])) {
@@ -8,7 +9,7 @@ if (!empty($_GET['code']) && isset($_GET['code'])) {
     $code = mysqli_real_escape_string($connection, $_GET['code']);
 
     // Verifica si hay un usuario con ese código en la base de datos
-    $urlComprobar = 'http://192.168.1.4/BackEnd/comprobarAct/' . $code;
+    $urlComprobar = $ip.'comprobarAct/' . $code;
     $jsonResponse = file_get_contents($urlComprobar);
     $data = json_decode($jsonResponse, true);
     //$c = mysqli_query($connection, "SELECT id_usuario, status FROM usuarios WHERE activacion='$code'");
@@ -18,26 +19,12 @@ if (!empty($_GET['code']) && isset($_GET['code'])) {
             $userId = $user['id_usuario'];
             $userStatus = $user['status'];
         }
-        //$userData = mysqli_fetch_assoc($c);
-        //$userId = $userData['id_usuario'];
-        //$userStatus = $userData['status'];
-
         if ($userStatus == '0') {
             // Si el usuario está inactivo, actualiza el estado a activo
-            $urlActivar = 'http://192.168.1.4/BackEnd/activar/';
             $datos = array(
                 'id' => $userId
             );
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $urlActivar);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-
-            $respuesta = curl_exec($ch);
-            curl_close($ch);
-            //mysqli_query($connection, "UPDATE usuarios SET status='1' WHERE id_usuario='$userId'");
+            activar($datos,$ip);
             $msg = "Tu cuenta ha sido activada";
         } else {
             $msg = "Tu cuenta ya está activa, no es necesario activarla nuevamente";

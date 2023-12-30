72a90d89a94977bff020f184949fe36812f3e7da
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 17:27:28 -0500

    Conexion con el back correctamente implementada

--- funcBack/activar.php
+++ funcBack/activar.php
@@ -1,13 +1,12 @@
 <?php
 function activar($datos, $ip){
-    $urlActivar = $ip.'activar';
+    $urlActivar = $ip.'activar/';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $urlActivar);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
     curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($datos));
-    curl_setopt($ch, CURLOPT_POST, true);
-
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
     $respuesta = curl_exec($ch);
     curl_close($ch);
     return $respuesta;

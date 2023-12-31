8d8ae494772359972c4a26b27712df367ed18e12
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 22:10:48 -0500

    Funcion para modificar la contraseña Se permite modificar la contraseña Se utiliza una conexion con el Back mediante una Api

--- funcBack/token.php
+++ funcBack/token.php
@@ -12,4 +12,18 @@ function tokenReset($datos,$ip){
     curl_close($ch);
     return $respuesta;
 }
+
+function cambiaClave($datos,$ip) {
+    $url = $ip.'clave/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($respuesta,true);
+}
 ?>

cc6e7393442e650ec48481fda43e4eb675f5a2b6
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 23:48:55 -0500

    Conexion con el Back Se conectó la obtención de datos con el Back

---
+++ logic/GestorUsuario.php
@@ -0,0 +1,80 @@
+<?php
+    
+    include 'funcBack/selectAll.php';
+    // Verificar si el usuario NO ha iniciado sesión
+    function verificarSesionNoIniciada() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+        if (!isset($_SESSION['user_id'])) {
+            // Si el usuario NO ha iniciado sesión, redirigir a la página "index"
+            header("Location: index.php");
+            exit();
+        }
+    }
+
+    // Verificar si el usuario ha iniciado sesión
+    function verificarSesionIniciada() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION['user_id'])) {
+            // Si el usuario ha iniciado sesión, redirigir a la página de "home"
+            header("Location: home.php");
+            exit();
+        }
+    }
+
+    // Obtener detalles del usuario basados en el user_id
+    function obtenerDetallesUsuario($user_id) {
+        include_once 'configs.php';
+        $datos = array(
+            'id'=>$user_id
+        );
+        $filas = selectAllID($datos,$ip);
+        return $filas;
+        /*global $connection;
+
+        $sql = "SELECT nombre, apellido, tipo_usuario FROM usuarios WHERE id_usuario = ?";
+        $stmt = mysqli_prepare($connection, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+        mysqli_stmt_execute($stmt);
+        $res = mysqli_stmt_get_result($stmt);
+        $fila = mysqli_fetch_assoc($res);*/
+    }
+
+    // Actualizar detalles del usuario
+    function actualizarDetallesUsuario($user_id, $nuevos_datos) {
+        global $connection;
+
+        // Lógica para actualizar detalles del usuario en la base de datos
+        // ...
+    }
+
+    // Cerrar sesión y redirigir al usuario a la página de inicio de sesión
+    function cerrarSesion() {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+        session_destroy();
+        header("Location: index.php");
+        exit();
+    }    
+
+    // Cambiar la contraseña del usuario
+    function cambiarContrasena($user_id, $nueva_contrasena) {
+        global $connection;
+
+        // Lógica para cambiar la contraseña del usuario en la base de datos
+        // ...
+    }
+
+    // Validar datos del formulario
+    function validarFormulario($datos_formulario) {
+        // Lógica para validar los datos del formulario antes de procesarlos
+        // ...
+    }
+?>

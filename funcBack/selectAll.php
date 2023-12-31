e2a484364e5b73d3fb4c109ef448cc845d9a66c1
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-30 23:52:33 -0500

    Funcion para traer nombre apellido y tipo por id Se creó una función que trae los datos necesarios dado el id_usuario

--- funcBack/selectAll.php
+++ funcBack/selectAll.php
@@ -43,4 +43,18 @@ function selectIdClave($datos,$ip) {
     return json_decode($respuesta,true);
 }
 
+function selectRestrictedID($datos,$ip)
+{
+    $url = $ip .'selectRestricted/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($respuesta,true);
+}
 ?>

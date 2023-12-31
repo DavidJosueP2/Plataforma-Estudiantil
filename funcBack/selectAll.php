868161fd96ff0e5b7ba89482f961a3a704de0bf0
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-31 12:03:41 -0500

    Funciones para seleccionar paralelos y materias Las funciones que seleccionan se comunican con el Back

--- funcBack/selectAll.php
+++ funcBack/selectAll.php
@@ -57,4 +57,34 @@ function selectRestrictedID($datos,$ip)
     curl_close($ch);
     return json_decode($respuesta,true);
 }
+
+function selectParalelos($datos,$ip)
+{
+    $url = $ip .'paralelos/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($respuesta,true);
+}
+
+function selectMaterias($datos,$ip)
+{
+    $url = $ip .'materias/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($respuesta,true);
+}
 ?>

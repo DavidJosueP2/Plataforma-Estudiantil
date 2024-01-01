e7a9ba22e6a9afde8dbb7b4fb96b4b03134f0b1c
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2023-12-31 19:09:55 -0500

    Archivo materias.php que conecta con el BackEnd El archivo materias.php tiene las funciones que conectan con el BackEnd Funciona correctamente

---
+++ funcBack/materias.php
@@ -0,0 +1,19 @@
+<?php
+function selectIDNivel($datos,$ip){
+    $url = $ip. 'niveles/';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($datos));
+    curl_setopt($ch, CURLOPT_POST, true);
+
+    $respuesta = curl_exec($ch);
+    curl_close($ch);
+    $resultado = json_decode($respuesta,true);
+    foreach ($resultado as $resultadito) {
+        $res = $resultadito;
+    }
+    return $res;
+}
+?>
